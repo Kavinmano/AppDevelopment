f6efed881f4c614f08de219879b47c21309352b6
Kavin Kumar K <110532169+Kavinmano@users.noreply.github.com>
2023-09-21 09:54:34 +0530

    Add files via upload

---
+++ Day 5/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+function Home() {
+    return (
+        <>
+
+            <Layout />
+            <div className='main-wrapper'>
+                <div className='welcome-container'>
+                    <h1> Home </h1>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Home
