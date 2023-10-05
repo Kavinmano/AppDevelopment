5ff2ff93b00bd52d0ae0b85dd73b8244bc4c9065
Kavin Kumar K <110532169+Kavinmano@users.noreply.github.com>
2023-10-05 10:10:11 +0530

    Add files via upload

---
+++ Day 12/src/main/java/com/kavin/main/dto/info/ProductInfo.java
@@ -0,0 +1,17 @@
+package com.kavin.main.dto.info;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductInfo {
+    private Long pid;
+    private Long quantity;
+}
+
